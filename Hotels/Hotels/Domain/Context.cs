3afc9babf2a4fdc70840ad5ab63e8b260c8418be
AlhagebAlmansoor <32272562+AlhagebAlmansoor@users.noreply.github.com>
2017-09-27 12:40:20 +0800

    NewChanges
    d

---
+++ Hotels/Hotels/Domain/Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using commLib;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Hotels.Domain
+{
+    public class ServiceContext : ZowarahContext
+    {
+
+        public DbSet<Hotels> Hotels { get; set; }
+    }
+
+    public class Hotels : AuditableEntity<long>
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
